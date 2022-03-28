60bea3663606b6520bb33218cd97a559cba27f46
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-28 12:31:34 -0700

    made render function

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,21 @@
 export function renderTodo(todo) {
+    const div = document.createElement('div');
+    const pTag = document.createElement('p');
     // create a div and a p tag
+
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
+    if (todo.complete) {
+        div.classList.add('complete');
+    } else {
+        div.classList.add('incomplete');
+    }
     // add the 'todo' css class no matter what
-
+    div.classList.add('todo');
     // put the todo's text into the p tag
+    todo.textContent = pTag;
 
     // append stuff
-
+    div.append(pTag);
     // return the div
+    return div;
 }
