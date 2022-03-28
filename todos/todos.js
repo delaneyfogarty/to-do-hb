86884b9a3208c85e9e39e58a6132c41e9185db09
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-28 12:57:27 -0700

    figuring out todos.js

--- todos/todos.js
+++ todos/todos.js
@@ -1,10 +1,10 @@
-import { 
-    checkAuth, 
-    createTodo, 
-    completeTodo,
-    getTodos,
-    logout,
-    deleteAllTodos, 
+import {
+  checkAuth,
+  createTodo,
+  completeTodo,
+  getTodos,
+  logout,
+  deleteAllTodos,
 } from '../fetch-utils.js';
 import { renderTodo } from '../render-utils.js';
 
@@ -15,13 +15,45 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
-todoForm.addEventListener('submit', async(e) => {
+todoForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const data = new FormData(todoForm);
+
+  await createTodo({
+    todo: data.get('todo'),
+    complete: false,
+  });
+
+  todoForm.reset();
+
+  await displayTodos();
     // on submit, create a todo, reset the form, and display the todos
 });
 
+
 async function displayTodos() {
     // fetch the todos
-    
+  todosEl.textContent = '';
+
+  const todoList = await getTodos();
+
+  for (let item of todoList) {
+    const itemEl = document.createElement('p');
+
+    itemEl.textContent = `${item.todo}`;
+
+    if (item.complete) {
+      itemEl.classList.add('is_complete');
+    } else {
+      itemEl.addEventListener('click', async () => {
+        await completeTodo(item.id);
+        displayTodos();
+
+      });
+    }
+    todoList.append(itemEl);
+  }
+
     // display the list of todos
 
     // be sure to give each todo an event listener
@@ -30,14 +62,18 @@ async function displayTodos() {
 }
 
 // add an on load listener that fetches and displays todos on load
+window.addEventListener('load', () => {
+    displayTodos();
+});
 
 logoutButton.addEventListener('click', () => {
-    logout();
+  logout();
 });
 
 
-deleteButton.addEventListener('click', async() => {
+deleteButton.addEventListener('click', async () => {
     // delete all todos
-
+  await deleteAllTodos();
     // then refetch and display the updated list of todos
+  await displayTodos();
 });
